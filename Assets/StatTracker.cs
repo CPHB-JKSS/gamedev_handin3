6b7efebb722ffb42f0f5e363051a228bb429cff0
A-R-G-Wagner <cph-aw116@cphbusiness.dk>
2021-02-23 12:32:22 +0100

    collisions, whole map, time counter

--- Assets/StatTracker.cs
+++ Assets/StatTracker.cs
@@ -6,43 +6,74 @@ using UnityEngine.UIElements;
 public class StatTracker : MonoBehaviour
 {
 
-    public GameObject startZone, endZone;
-    private TimerState timer;
+    public GameObject startZone, endZone, walls;
+    //private TimerState timer;
+    public bool gameStarted;
+    public float gameTimer;
+    public int wallHits;
 
+    void OnCollisionEnter(Collision collider)
+    {
 
-    void OnTriggerEnter(Collider collider) {
+        if ((collider.gameObject.name.Contains("Wall") || collider.gameObject.name.Contains("Cylinder")) && gameStarted)
+        {
+            wallHits++;
+            Debug.Log(walls.gameObject.name + " HIT! oof...");
+        }
+
+        //Debug.Log("COLLISION: " + collider.gameObject.name);
+    }
+    void OnTriggerEnter(Collider collider)
+    {
         // If entering endzone
-        if (collider.gameObject.name == endZone.name) { 
-            Debug.Log(timer.deltaTime);
+        if (collider.gameObject.name == endZone.name && gameStarted)
+        {
+            //Debug.Log(timer.deltaTime);
+            gameStarted = false;
+
+            Debug.Log("Game ended!");
+            Debug.Log("Game lasted: " + gameTimer);
+            Debug.Log("Walls hit: " + wallHits);
         }
 
-
-
-        Debug.Log("Enter: " + collider.gameObject.name);
+        
+        //Debug.Log("Enter: " + collider.gameObject.name);
     }
 
-    void OnTriggerStay(Collider collider) {
+    void OnTriggerStay(Collider collider)
+    {
 
     }
 
-    void OnTriggerExit(Collider collider) {
+    void OnTriggerExit(Collider collider)
+    {
         // If exitting startzone
-        if (collider.gameObject.name == startZone.name) { 
-            Debug.Log(timer.now);
+        if (collider.gameObject.name == startZone.name && !gameStarted)
+        {
+            gameStarted = true;
+            Debug.Log("Game started!");
 
 
-            Debug.Log("Exit: " + collider.gameObject.name);
+            //Debug.Log("Exit: " + collider.gameObject.name);
         }
     }
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameTimer = 0;
+        gameStarted = false;
+        wallHits = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameStarted)
+        {
+            gameTimer++;
+        }
+
     }
 }
